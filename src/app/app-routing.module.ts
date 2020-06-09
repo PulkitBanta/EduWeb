6cd7b97fd459461d8dc425b248e53ffdfb1b198d
Pulkit <Pulkitbanta55555@gmail.com>
2020-06-09 16:55:21 +0530

    login and signup forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
-  // { paht: 'login', component: LoginComponent },
-  // { paht: 'platform', component: PlatformComponent },
-  // { paht: 'platform/session', component: SessionComponent },
-  // { paht: 'platform/download', component: DownloadsComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  // { path: 'platform', component: PlatformComponent },
+  // { path: 'platform/session', component: SessionComponent },
+  // { path: 'platform/download', component: DownloadsComponent }
 ];
 
 @NgModule({
