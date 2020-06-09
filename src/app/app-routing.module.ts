a3f49998ce47eac4a424cc37d8bc99fc719e8953
Pulkit <Pulkitbanta55555@gmail.com>
2020-06-09 15:01:01 +0530

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  // { paht: 'login', component: LoginComponent },
+  // { paht: 'platform', component: PlatformComponent },
+  // { paht: 'platform/session', component: SessionComponent },
+  // { paht: 'platform/download', component: DownloadsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
