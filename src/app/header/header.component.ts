592835dc9c2652f65c896df7d71a63e5835ca998
Pulkit <Pulkitbanta55555@gmail.com>
2020-06-16 13:51:46 +0530

    Code refactor

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../authentication.service'
-import { Observable } from 'rxjs'
 
 @Component({
   selector: 'app-header',
